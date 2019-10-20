612f65ece5eed29ab73a8ac915185c6a8e9456d1
Natalia Kirdis <kirdisnatalia@gmail.com>
2019-10-20 17:00:31 +0300

    Максимум из двух чисел [#188644]. Добавлены тесты.

--- chapter001/src/test/java/ru/job4j/condition/MaxTest.java
+++ chapter001/src/test/java/ru/job4j/condition/MaxTest.java
@@ -6,8 +6,26 @@ import static org.junit.Assert.assertThat;
 
 public class MaxTest {
   @Test
-  public void whenMax1To2Then2() {
+  public void whenFirstMax() {
+    int result = Max.max(3, 2);
+    assertThat(result, is(3));
+  }
+
+  @Test
+  public void whenSecondMax() {
     int result = Max.max(1, 2);
     assertThat(result, is(2));
+  }
+
+  @Test
+  public void whenEquals() {
+    int result = Max.max(3, 3);
+    assertThat(result, is(3));
+  }
+
+  @Test
+  public void whenNegativeNumbers() {
+    int result = Max.max(-1, -2);
+    assertThat(result, is(-1));
   }
 }
