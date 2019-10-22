2281680b212305c4cf60548e7eb9e540b9db96cf
Natalia Kirdis <kirdisnatalia@gmail.com>
2019-10-22 08:11:34 +0300

    Простое число. [#188660]

---
+++ chapter001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,14 @@
+package ru.job4j.loop;
+
+public class CheckPrimeNumber {
+  public boolean check(int num) {
+    boolean isPrime = true;
+    for (int i = 2; i <= num / 2 ; i ++) {
+      if (num % i == 0) {
+        isPrime = false;
+        break;
+      }
+    }
+    return isPrime;
+  }
+}
