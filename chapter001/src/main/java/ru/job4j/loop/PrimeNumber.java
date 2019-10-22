380d844699a610b3755aa761e8f0a91eb8592649
Natalia Kirdis <kirdisnatalia@gmail.com>
2019-10-22 13:37:27 +0300

    Простые числа. [#188658] Использован метод проверки числа нато, что оно натуральное

--- chapter001/src/main/java/ru/job4j/loop/PrimeNumber.java
+++ chapter001/src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -3,17 +3,10 @@ package ru.job4j.loop;
 public class PrimeNumber {
   public int calc(int finish) {
     int count = 0;
+    CheckPrimeNumber checkPrimeNumber = new CheckPrimeNumber();
 
-
-    for (int j = 2; j <=finish; j++) {
-      boolean isPrime = true;
-      for (int i = 2; i <= j / 2; i++) {
-        if (j % i == 0) {
-          isPrime = false;
-          break;
-        }
-      }
-      if (isPrime == true) {
+    for (int j = 2; j <= finish; j++) {
+      if (checkPrimeNumber.check(j)) {
         count++;
       }
     }
