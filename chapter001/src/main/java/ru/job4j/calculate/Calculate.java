47909d76a344f425b940fb6e1cc16ab43d7904c9
Natalia Kirdis <kirdisnatalia@gmail.com>
2019-10-22 18:21:14 +0300

    JavaDoc. Code convention [#188655]

---
+++ chapter001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -0,0 +1,17 @@
+package ru.job4j.calculate;
+
+/**
+ * Calculate
+ *
+ * @author Natalia Kirdis (kirdisnatalia@gmail.com)
+ */
+public class Calculate {
+
+  /**
+   * Main
+   * @param args
+   */
+  public static void main(String[] args) {
+
+  }
+}
