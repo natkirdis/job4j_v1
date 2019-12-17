0f094d0a5f73f8141dfaa71cf646df522808ddfb
Natalia Kirdis <kirdisnatalia@gmail.com>
2019-12-17 23:46:54 +0300

    Рефакторинг - Расстояние между точками.[#188787]

--- chapter001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter001/src/test/java/ru/job4j/condition/PointTest.java
@@ -2,42 +2,44 @@ package ru.job4j.condition;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
-import static ru.job4j.condition.Point.distance;
+//import static ru.job4j.condition.Point;
 
 public class PointTest {
   @Test
   public void pointPositiveTest() {
-    int[] coordinates = {0, 0, 2, 0};
+
+    Point a = new Point(0, 0);
+    Point b = new Point(0, 2);
 
     double expected = 2.0;
-    double out = distance(coordinates[0], coordinates[1], coordinates[2], coordinates[3]);
+    double out = a.distance(b);
     assertEquals(expected, out, 0.01);
   }
 
   @Test
   public void pointNegativeVectorTest() {
-    int[] coordinates = {5, -1, 10, -5};
-
+    Point a = new Point(5, -1);
+    Point b = new Point(10, -5);
     double expected = 6.4031242374328485;
-    double out = distance(coordinates[0], coordinates[1], coordinates[2], coordinates[3]);
+    double out = a.distance(b);
     assertEquals(expected, out, 0.01);
   }
 
   @Test
   public void pointPositiveVectorTest() {
-    int[] coordinates = {-5, 1, -10, 5};
-
+    Point a = new Point(-5, 1);
+    Point b = new Point(-10, 5);
     double expected = 6.4031242374328485;
-    double out = distance(coordinates[0], coordinates[1], coordinates[2], coordinates[3]);
+    double out = a.distance(b);
     assertEquals(expected, out, 0.01);
   }
 
   @Test
   public void pointNullDistanceTest() {
-    int[] coordinates = {0, 0, 0, 0};
-
-    double expected = 0.0;
-    double out = distance(coordinates[0], coordinates[1], coordinates[2], coordinates[3]);
+    Point a = new Point(0, 0);
+    Point b = new Point(0, 0);
+    double expected = 0;
+    double out = a.distance(b);
     assertEquals(expected, out, 0.01);
   }
 }
