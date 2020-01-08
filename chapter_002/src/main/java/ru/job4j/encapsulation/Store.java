a1c9adcea96a59da10c22b36909695d898e140f5
Natalia Kirdis <kirdisnatalia@gmail.com>
2020-01-08 18:34:21 +0300

    Удаление моделей из массива.[#188815]

--- chapter_002/src/main/java/ru/job4j/encapsulation/Store.java
+++ chapter_002/src/main/java/ru/job4j/encapsulation/Store.java
@@ -1,36 +1,73 @@
 package ru.job4j.encapsulation;
 
+import sun.awt.util.IdentityLinkedList;
+
 public class Store {
+
+  public static Product[] delete(Product[] products, int index) {
+    for (int i = index; i < products.length - 2; i++) {
+      products[i] = products[i + 1];
+    }
+    products[products.length - 1] = null;
+    return products;
+  }
+
   public static void main(String[] args) {
-    Product milk = new Product("Milk", 10);
-    Product bread = new Product("Bread", 4);
-    Product egg = new Product("Egg", 19);
+    Product products[] = new Product[5];
+    products[0] = new Product("Milk", 10);
+    products[1] = new Product("Bread", 4);
+    products[2] = new Product("Egg", 19);
 
-    Product[] prods = new Product[3];
-
-    prods[0] = milk;
-    prods[1] = bread;
-    prods[2] = egg;
-
-    for (int index = 0; index < prods.length; index++) {
-      Product pr = prods[index];
-      System.out.println(pr.getName() + " - " + pr.getCount());
+    for (int i = 0; i < products.length; i++) {
+      Product product = products[i];
+      //проверяем, что объект не равен null. тк у нас массив не заполнен целиком.
+      if (product != null) {
+        System.out.println(product.getName());
+      } else {
+        System.out.println("null");
+      }
     }
 
-    System.out.println("Replace milk to oil.");
-    Product oil = new Product("Oil", 11);
-    prods[0] = oil;
+    System.out.println();
+    System.out.println("Удаляем значение из ячейки с индексом 1");
+    //удаляем значение из ячейки с индексом 1
+    products[1] = null;
 
-    for (int index = 0; index < prods.length; index++) {
-      Product pr = prods[index];
-      System.out.println(pr.getName() + " - " + pr.getCount());
+    for (int i = 0; i < products.length; i++) {
+      Product product = products[i];
+      //проверяем, что объект не равен null. тк у нас массив не заполнен целиком.
+      if (product != null) {
+        System.out.println(product.getName());
+      } else {
+        System.out.println("null");
+      }
     }
 
-    System.out.println("Shown only product.count > 10");
-    for (int index = 0; index < prods.length; index++) {
-      Product pr = prods[index];
-      if (pr.getCount() > 10) {
-        System.out.println(pr.getName() + " - " + pr.getCount());
+    System.out.println();
+    System.out.println("Записываем  в ячейку с индексом 1 значение ячейки с индексом 2 и удаляем значение из ячейки с индексом 2");
+    //записываем в ячейку с индексом 1 значение ячейки с индексом 2.
+    products[1] = products[2];
+    //удаляем значение из ячейки с индексом 2.
+    products[2] = null;
+    for (int i = 0; i < products.length; i++) {
+      Product product = products[i];
+      //проверяем, что объект не равен null. так как  у нас массив не заполнен целиком.
+      if (product != null) {
+        System.out.println(product.getName());
+      } else {
+        System.out.println("null");
+      }
+    }
+
+    System.out.println("Удаляем значение из ячейки с индексом 0");
+    delete(products, 0);
+    for (int i = 0; i < products.length; i++) {
+      Product product = products[i];
+      //проверяем, что объект не равен null. так как  у нас массив не заполнен целиком.
+      if (product != null) {
+        System.out.println(product.getName());
+      } else {
+        System.out.println("null");
       }
     }
   }
